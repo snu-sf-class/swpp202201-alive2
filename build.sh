0b78a2af0fbb47ee8a88a60cdb02bec2cbbfffa1
strikef <seunghyeon.nam@sf.snu.ac.kr>
2022-04-20 15:29:16 +0900

    Default build script for CI

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Specify LLVM installation directory
+LLVM_DIR=/opt/llvm
+# Specify Z3 installation directory (Z3 will be installed here!)
+Z3_DIR=/opt/z3
+
+# Build Alive2
+cmake -GNinja -Bbuild \
+    -DLLVM_ROOT=$LLVM_DIR \
+    -DZ3_ROOT=$Z3_DIR \
+    -DBUILD_TV=ON \
+    -DCMAKE_BUILD_TYPE=Release
+cmake --build build
